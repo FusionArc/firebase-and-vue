6d6a7803ba6ecbe499ca71cb1b3b17d7d566b645
FusionArc <dave@fusion-arc.uk>
2020-06-07 15:43:31 +0100

    Update HomeCard.vue

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -1,22 +1,37 @@
 /* eslint-disable */
 <template>
-  <v-card class="v-flex intro">
+  <v-card class="intro dense">
     <div class="heading">
       <v-card-title>
-        <h1>Welcome to CatScrapper.com</h1>
+        <h2>Cat Scrapper</h2>
+        <h2>
+          Precious Metal<br />
+          Recovery & Refining
+        </h2>
       </v-card-title>
-      <v-card-subtitle>
-        <h2>Recyclers & processors of retired catalytic convertors</h2>
-      </v-card-subtitle>
+      <v-card-subtitle> </v-card-subtitle>
     </div>
     <div class="card-body">
       <v-card-text>
+        <h2>Recyclers & processors of retired catalytic convertors</h2>
         <p>
           We aim to provide a bespoke <span><em>Cats to Cash</em></span> service
-          for commercial & hobbyist recyclers<br /><br />
-          For all enthusiasts and collectors please browse out site where you
-          will find current catalytic convertor recyling news.<br /><br />
-          Checkout the realtime precious metal prices
+          for commercial & hobbyist recyclers
+        </p>
+        <br />
+        <p>
+          For all enthusiasts and collectors please browse our site where you<br />
+          will find current catalytic convertor recyling news & insights.<br />
+        </p>
+        <p>Checkout the realtime precious metal prices.<br /></p>
+        <p>
+          Share and Learn about precious metal recovery in 2020<br /><br />
+          Be part of growing community of scrappers, enthusiasts & experts of
+          their field.<br />
+        </p>
+        <p>
+          Check our <em>`cat a list`</em> to match your salvaged collections.<br />
+          Match model codes against our lists for fast valuation of your items.
         </p>
       </v-card-text>
     </div>
@@ -34,28 +49,39 @@ export default {
 </script>
 <style scoped>
 .heading {
-  background-color: burlywood;
+  text-align: center;
+  background-image: linear-gradient(
+    to top,
+    rgba(255, 255, 0, 0.5),
+    rgba(0, 0, 255, 0.5)
+  );
+  padding: 0;
 }
 .intro {
   font-family: Verdana, Geneva, Tahoma, sans-serif;
+  grid-column: 2 / -2;
 }
-.intro h1 {
+h2 {
   text-align: center;
   min-width: 100%;
-  text-shadow: 0 0 3px #0000dd;
-  padding: 0.25em;
+  padding: 0;
+  line-height: 1.4;
+  font-family: serif;
+  font-variant: small-caps;
 }
 .intro h2 {
   text-align: center;
-  padding: 0.5em;
+  padding: 0;
+  font-family: serif;
+  font-variant: small-caps;
 }
 .intro p {
   font-family: serif;
   font-weight: 500;
   color: rgb(5, 63, 63);
   text-align: center;
-  padding: 0.5em;
-  line-height: 1.5;
+  line-height: 1.25;
+  font-variant: small-caps;
   font-size: 1.75em;
 }
 .card-body {
@@ -67,6 +93,10 @@ export default {
     url(../../public/img/icons/bullions.png);
   background-size: contain;
   background-position: center;
+}
+.card-body {
+  color: rgb(5, 63, 63);
+  padding-bottom: 5em;
 }
 .card-body img {
   min-width: 4em;
